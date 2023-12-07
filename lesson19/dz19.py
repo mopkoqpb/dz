c1c61e12f4ca23f8c92bb128d8528dff4410742e
андрей <mopkoqpb95@gmail.com>
2023-12-07 17:48:21 +0300

    add comit

--- lesson19/dz19.py
+++ lesson19/dz19.py
@@ -1,12 +1,14 @@
 def debug(func):
     def wrap(*args,**kwargs):
-        print('Name func: ', func.__name__, 'call',*args,**kwargs)
+        print('Name func: ', func.__name__, 'call',*args,{**kwargs})
         res = func(*args,**kwargs)
-        print(res)
+        print('Вернул значение', func.__name__, res)
         return res
     return wrap
 @debug
-def mf(x):
+def mf(*args):
+    x = sum(args)
     return x
-mf(1)
+
+mf(1,23,3,5,6)
 
